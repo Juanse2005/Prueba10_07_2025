5eccffa3796368b32dd757c3c1b0801b4298f701
Juanse2005 <juannino907@gmail.com>
2025-07-10 22:02:54 -0500

    [JN] -  Added user not found exception for error message // Se agrego la excepcion usuario no encontrado para mensaje de error

---
+++ backend/src/main/java/com/prueba/backend/domain/exception/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package com.prueba.backend.domain.exception;
+
+public class UserNotFoundException extends RuntimeException{
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
